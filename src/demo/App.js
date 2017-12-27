46838eda6bb3062ca81422fe18e43f3e15d8bafa
Dimitri Mikadze <dimitrimikadze@gmail.com>
2017-12-27 15:49:51 +0400

    Testing new version of Create react library

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import Example from '../lib';
-import { SecondExample } from '../lib';
+import UnderConstruction from '../lib';
 
 const App = () => (
-  <div>
-    <Example />
-    <SecondExample />
-  </div>
+  <UnderConstruction
+    background={{
+      image: 'https://static.pexels.com/photos/259698/pexels-photo-259698.jpeg',
+      textColor: '#fff',
+      overlay: {
+        color: '#000',
+        opacity: '.5'
+      }
+    }}
+    logo={{
+      src: 'https://image.ibb.co/kmtJK5/Rubby_white_lines.png',
+      alt: 'alt text',
+      style: {
+        width: '160px'
+      }
+    }}
+    description={{
+      text: 'Our website is under construction. We\'ll be here soon, subscribe to be notified',
+      style: {
+        maxWidth: '440px',
+      }
+    }}
+    subscribe={{
+      placeholder: 'Enter your email',
+      buttonText: 'Subscribe',
+      onSubmit: (value) => {
+        console.log('user typed email :', value);
+      }
+    }}
+    links={[
+      {
+        url: 'https://www.facebook.com/',
+        image: 'https://image.flaticon.com/icons/svg/220/220200.svg',
+      },
+      {
+        url: 'https://www.twitter.com/',
+        image: 'https://image.flaticon.com/icons/svg/145/145812.svg',
+      },
+      {
+        url: 'https://www.linkedin.com/',
+        image: 'https://image.flaticon.com/icons/svg/145/145807.svg',
+      },
+      {
+        url: 'mailto:someone@example.com',
+        image: 'https://image.flaticon.com/icons/svg/321/321817.svg',
+      },
+    ]}
+  />
 );
 
 export default App;
